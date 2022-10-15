77805e1ed2d87d3814032ff6cd2ede51350ea47f
sayedppqq <sayedmmnn0@gmail.com>
2022-10-15 22:24:15 +0600

    first commit

---
+++ ATMapp/App/ATMapp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ATMapp
+{
+    class ATMapp
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="My ATM App";
+            Console.WriteLine("Wellcome to ATM App");
+            Console.WriteLine("Press enter to continue.....");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ATMapp/UI/AppScrean.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMapp.UI
+{
+    public static class AppScrean
+    {
+        internal static void Welcome()
+        {
+            Console.Clear();
+            Console.Title = "My ATM App";
+            Console.WriteLine("Wellcome to ATM App");
+            PressEnterToContinue();
+        }
+
+        private static void PressEnterToContinue()
+        {
+            Console.WriteLine("Press enter to continue.....");
+            Console.ReadLine();
+        }
+    }
+}
